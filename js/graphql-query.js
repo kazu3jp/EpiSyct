d1b8e932056499383fbc9f891af0b3087270583f
kazu3jp <kazu3jp.purin@outlook.jp>
2023-05-05 15:37:26 +0900

    なんかよくわからん。

--- js/graphql-query.js
+++ js/graphql-query.js
@@ -1,47 +1,47 @@
-var query
-    = 'query {'
-    + 'viewer {'
-    + 'libraryEntries (states: WATCHING) {'
-    + 'nodes {'
-    + 'work{'
-    + 'id,'
-    + 'annictId,'
-    + 'seasonName,'
-    + 'seasonYear,'
-    + 'title,'
-    + 'officialSiteUrl,'
-    + 'media,'
-    + 'episodesCount,'
-    + 'noEpisodes,'
-    + 'image{'
-    + 'recommendedImageUrl'
-    + '},'
-    + 'episodes(orderBy: {'
-    + 'direction: ASC, field: SORT_NUMBER'
-    + '}) {'
-    + 'nodes{'
-    + 'numberText,'
-    + 'title,'
-    + 'viewerDidTrack,'
-    + 'id,'
-    + 'annictId'
-    + '}'
-    + '}'
-    + 'nextEpisode{'
-    + 'numberText,'
-    + 'title,'
-    + 'viewerDidTrack,'
-    + 'id,'
-    + 'annictId,'
-    + '}'
-    + 'nextProgram{'
-    + 'startedAt,'
-    + 'channel{'
-    + 'annictId,'
-    + 'name,'
-    + '}'
-    + '}'
-    + '}'
-    + '}'
-    + '}'
-    + '}'
+var query = `query {
+    viewer {
+        libraryEntries(states: WATCHING) {
+            nodes {
+				work{
+                    id,
+                        annictId,
+                        seasonName,
+                        seasonYear,
+                        title,
+                        officialSiteUrl,
+                        media,
+                        episodesCount,
+                        noEpisodes,
+                        image{
+                        recommendedImageUrl
+                    },
+                    episodes(orderBy: {
+                        direction: ASC, field: SORT_NUMBER
+                    }) {
+						nodes{
+                            numberText,
+                                title,
+                                viewerDidTrack,
+                                id,
+                                annictId
+                        }
+                    }
+                }
+                nextEpisode{
+                    numberText,
+                        title,
+                        viewerDidTrack,
+                        id,
+                        annictId,
+			    }
+			    nextProgram{
+                    startedAt,
+                        channel{
+                        annictId,
+                            name,
+			        }
+                }
+            }
+        }
+    }
+}`
